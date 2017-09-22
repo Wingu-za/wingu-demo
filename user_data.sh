4ca28d13191d954af491744470cda0a8d51731a2
Allan <allanice001@users.noreply.github.com>
2017-09-22 06:02:28 +0200

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,3 +1,8 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install git python-minimal ansible -y
-git clone https://github.com/TachyonProject/ansible-django
+#!/bin/bash -v
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+sudo export DEBIAN_FRONTEND=noninteractive apt update -y
+sudo export DEBIAN_FRONTEND=noninteractive apt upgrade -y
+sudo export DEBIAN_FRONTEND=noninteractive apt install git ansible -y
+git clone https://github.com/TachyonProject/ansible-django-stack
+cd ~/ansible-django-stack & ansible-playbook -vvvv -i inventory.ini ansible/playbooks/playbook-install-python2.yml
+cd ~/ansible-django-stack & ansible-playbook -vvvv -i ansible/inventory.ini ansible/playbooks/playbook-all.yml
