84a19249cc2cbf88194c7ee6843d4daaa8c85fe7
Christiaan Rademan <christiaan.rademan@gmail.com>
2017-09-27 08:48:48 +0200

    removed user_data.sh its defined in hot template

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # wingu-demo
+
+HEAT TEMPLATE URL:
+------------------
+
+https://raw.githubusercontent.com/TachyonProject/wingu-demo/master/demo.hot
+
--- user_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -v
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-export DEBIAN_FRONTEND=noninteractive 
-sudo apt update -y
-sudo apt install git ansible -y
-git clone https://github.com/TachyonProject/ansible-django-stack
-cd ~/ansible-django-stack & ansible-playbook -vvvv -i inventory.ini ansible/playbooks/playbook-install-python2.yml
-cd ~/ansible-django-stack & ansible-playbook -vvvv -i ansible/inventory.ini ansible/playbooks/playbook-all.yml
