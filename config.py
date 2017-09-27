58ddebb2299bfb3305305dff5a635f9458ba5214
Christiaan Rademan <christiaan.rademan@gmail.com>
2017-09-27 10:47:10 +0200

    Upload

---
+++ config.py
@@ -0,0 +1,5 @@
+AUTH_URL = 'https://identity.wingu.co.za:5000/v2.0'                                  
+USERNAME = 'wingu-demo@tachyonic.co.za'
+PASSWORD = '@vage-2017'
+PROJECT_ID = 'c97ad1c671ec466c9c47437fa1496f54'
+HEAT_URL = 'https://orchestration.wingu.co.za:8004/v1/c97ad1c671ec466c9c47437fa1496f54'
---
+++ delete.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+from keystoneauth1 import loading
+from keystoneauth1 import session
+from heatclient import client as heatclient
+from neutronclient.v2_0 import client as neutronclient
+
+from config import AUTH_URL, USERNAME, PASSWORD, PROJECT_ID
+
+loader = loading.get_plugin_loader('password')
+auth = loader.load_from_options(auth_url=AUTH_URL,
+                                username=USERNAME,
+                                password=PASSWORD,
+                                project_id=PROJECT_ID)
+sess = session.Session(auth=auth)
+heat = heatclient.Client('1', session=sess)
+neutron = neutronclient.Client(session=sess)
+
+
+stack_name = "testing_demo"
+
+# Find Stack ID
+stacks = heat.stacks.list()
+for stack in stacks:
+    if stack.stack_name == stack_name:
+        stack_id = stack.id
+
+# Delete Stack
+heat.stacks.delete(stack_id)
